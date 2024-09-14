c3262fc807947f0bf9e85f3eb0a7ecfbc3c615fd
Aidan Beckett <aidanbeckett@aidans-air-2.lan>
2024-09-13 22:07:08 -0400

    poking around with lcd

--- lcd_test.py
+++ lcd_test.py
@@ -4,10 +4,10 @@ import time
 
 def setup():
 	LCD2004.init(0x27, 1)	# init(slave address, background light)
-	LCD2004.write(0, 0, 'Hello, world')
-	LCD2004.write(0, 1, 'IIC/I2C LCD2004')
-	LCD2004.write(0, 2, '20 cols, 4 rows')
-	LCD2004.write(0, 3, 'www.sunfounder.com')
+	LCD2004.write(6, 0, 'Blacks Turn')
+	# LCD2004.write(0, 1, 'IIC/I2C LCD2004')
+	# LCD2004.write(0, 2, '20 cols, 4 rows')
+	LCD2004.write(0, 3, '<-- press when finished with move')
 	time.sleep(2)
 
 def destroy():
