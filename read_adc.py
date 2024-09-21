21b4a7d38746af4364e8ef1fcb89ec740d4bb8f4
Aidan Beckett <aidanbeckett@aidans-air-2.lan>
2024-09-21 12:53:46 -0400

    fixed single adc read script

--- read_adc.py
+++ read_adc.py
@@ -1,6 +1,11 @@
 import spidev
 import time
 import numpy as np
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
 
 # Initialize SPI
 spi = spidev.SpiDev()
@@ -18,7 +23,20 @@ def convert_to_voltage(adc_value, vref=3.3):
     return (adc_value / 1023.0) * vref
 
 def main():
-    j = input("Which ADC input?")
-    while True:
-        print(convert_to_voltage(read_adc(j)))
-        time.sleep(0.25)
+    try:
+        j = input("Which ADC input?")
+        count=0
+        while True:
+            if count%2 == 0:
+                GPIO.output(17, GPIO.HIGH)
+            else: GPIO.output(17, GPIO.LOW)
+            time.sleep(0.25)
+            print(convert_to_voltage(read_adc(j)))
+            count += 1
+    except KeyboardInterrupt:
+        print("Exiting program")
+    finally:
+        GPIO.cleanup()
+
+if __name__ == "__main__":
+    main()
